253579f0914016a3b1cc3c93a836dabae09f5eb5
radiohead225l <radiohead225l@77e49527-3367-abf7-0bbe-53a63c11a891>
2014-04-22 17:33:04 +0000

    analizador lexico version estable

---
+++ trunk/analizadorLexicoTLF2/src/interfaz/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Este paquete contiene la interfaz de usuario
+ */
+/**
+* @author Jorge Leonardo Aguirre Martinez
+ * @author Luis Alberto Delgado Ortiz
+ * 
+ * @version 2.0
+ *
+ */
+package interfaz;
---
+++ trunk/analizadorLexicoTLF2/src/mundo/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Este paquete contiene las clases necesarias para desarrollar la logica 
+ * del analizador lexico
+ */
+/**
+ * @author Jorge Leonardo Aguirre Martinez
+ * @author Luis Alberto Delgado Ortiz
+ * 
+ * @version 2.0
+ *
+ */
+package mundo;
