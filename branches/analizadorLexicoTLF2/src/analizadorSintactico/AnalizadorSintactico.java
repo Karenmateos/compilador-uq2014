29840422a41dc0e59a3105c57db0903e7b34256a
radiohead225l <radiohead225l@77e49527-3367-abf7-0bbe-53a63c11a891>
2014-04-26 16:35:29 +0000

    clase analizador sintactico

--- branches/analizadorLexicoTLF2/src/analizadorSintactico/AnalizadorSintactico.java
+++ branches/analizadorLexicoTLF2/src/analizadorSintactico/AnalizadorSintactico.java
@@ -5,9 +5,50 @@ import java.util.ArrayList;
 import analizadorLexico.SimboloLexico;
 
 public class AnalizadorSintactico {
+	
 	private ArrayList<SimboloLexico> listaSimbolosLexicos;
 	private ArrayList<ErrorSintactico> listaErroresSintacticos;
 	private SimboloLexico tokenActual;
 	private int indice;
 	private UnidadDeCompilacion miUnidadDeCompilacion;
+
+
+/**
+ * Constructor de la clase AnalizadorSintactico
+ * @param listaSimbolos
+ */
+public AnalizadorSintactico(ArrayList<SimboloLexico> listaSimbolos)
+{
+	this.listaSimbolosLexicos = listaSimbolos;
+	listaErroresSintacticos = new ArrayList<ErrorSintactico>();
+	indice = 0;
+	tokenActual = listaSimbolosLexicos.get(indice);
+	miUnidadDeCompilacion = esUnidadDeCompilacion();
+}
+
+/**
+ * Obtiene el siguiente token de la lista de simbolos lexicos
+ */
+public void darSiguienteToken()
+{
+	if(indice == listaSimbolosLexicos.size()-1)
+	{
+		return;
+
+	}else
+	{
+		indice++;
+		tokenActual = listaSimbolosLexicos.get(indice);
+	}
+}
+
+/**
+ * Realiza Backtracking - devuelve la posicion y el token anterior
+ * @param posBT
+ */
+public void realizarBactracking(int posBT)
+{
+	indice = posBT;
+	tokenActual = listaSimbolosLexicos.get(indice);
+}
 }
