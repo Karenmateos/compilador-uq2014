bccd326b06289f0f7425bb4cffbd4f10ded422c2
radiohead225l <radiohead225l@77e49527-3367-abf7-0bbe-53a63c11a891>
2014-04-30 14:10:16 +0000

    Analizador sintactico

--- branches/analizadorLexicoTLF2/src/analizadorSintactico/AnalizadorSintactico.java
+++ branches/analizadorLexicoTLF2/src/analizadorSintactico/AnalizadorSintactico.java
@@ -2,53 +2,393 @@ package analizadorSintactico;
 
 import java.util.ArrayList;
 
+import analizadorLexico.Configuracion;
 import analizadorLexico.SimboloLexico;
 
 public class AnalizadorSintactico {
-	
+
 	private ArrayList<SimboloLexico> listaSimbolosLexicos;
 	private ArrayList<ErrorSintactico> listaErroresSintacticos;
 	private SimboloLexico tokenActual;
 	private int indice;
 	private UnidadDeCompilacion miUnidadDeCompilacion;
 
+	/**
+	 * Constructor de la clase AnalizadorSintactico
+	 * 
+	 * @param listaSimbolos
+	 */
+	public AnalizadorSintactico(ArrayList<SimboloLexico> listaSimbolos) {
+		this.listaSimbolosLexicos = listaSimbolos;
+		listaErroresSintacticos = new ArrayList<ErrorSintactico>();
+		indice = 0;
+		tokenActual = listaSimbolosLexicos.get(indice);
+		miUnidadDeCompilacion = esUnidadDeCompilacion();
+	}
 
-/**
- * Constructor de la clase AnalizadorSintactico
- * @param listaSimbolos
- */
-public AnalizadorSintactico(ArrayList<SimboloLexico> listaSimbolos)
-{
-	this.listaSimbolosLexicos = listaSimbolos;
-	listaErroresSintacticos = new ArrayList<ErrorSintactico>();
-	indice = 0;
-	tokenActual = listaSimbolosLexicos.get(indice);
-	miUnidadDeCompilacion = esUnidadDeCompilacion();
-}
+	/**
+	 * Obtiene el siguiente token de la lista de simbolos lexicos
+	 */
+	public void darSiguienteToken() {
+		if (indice == listaSimbolosLexicos.size() - 1) {
+			return;
 
-/**
- * Obtiene el siguiente token de la lista de simbolos lexicos
- */
-public void darSiguienteToken()
-{
-	if(indice == listaSimbolosLexicos.size()-1)
-	{
-		return;
+		} else {
+			indice++;
+			tokenActual = listaSimbolosLexicos.get(indice);
+		}
+	}
 
-	}else
-	{
-		indice++;
+	/**
+	 * Realiza Backtracking - devuelve la posicion y el token anterior
+	 * 
+	 * @param posBT
+	 */
+	public void realizarBactracking(int posBT) {
+		indice = posBT;
 		tokenActual = listaSimbolosLexicos.get(indice);
 	}
-}
 
-/**
- * Realiza Backtracking - devuelve la posicion y el token anterior
- * @param posBT
- */
-public void realizarBactracking(int posBT)
-{
-	indice = posBT;
-	tokenActual = listaSimbolosLexicos.get(indice);
-}
+	/**
+	 * Almacena un error sintactico
+	 * 
+	 * @param msn
+	 * @param fila
+	 * @param columna
+	 */
+	public void reportarError(String msn, int fila, int columna) {
+		listaErroresSintacticos.add(new ErrorSintactico(msn, fila, columna));
+	}
+
+	/**
+	 * Indica si cierto conjunto de tokens pertenecen a la categoria sintactica
+	 * Argumento GIC:<Argumento>::=<TipoDato> identificadorVariable
+	 * 
+	 * @return parametro
+	 */
+	public Argumento esArgumento() {
+		SimboloLexico tipoDato = null;
+		SimboloLexico identificadorVariable = null;
+
+		//tipo dato
+
+		if (tokenActual.getTipo().equals(Configuracion.IdVariable)) {
+			identificadorVariable = tokenActual;
+			darSiguienteToken();
+			return new Argumento(tipoDato, identificadorVariable);
+		} else {
+			if (tokenActual.getTipo().equals(Configuracion.IdMetodo)) {
+
+				reportarError(
+						"El identificador corresponde a un identificador de metodo",
+						tokenActual.getFila(), tokenActual.getColumna());
+				return new Argumento(tipoDato, identificadorVariable);
+
+			} else if (tokenActual.getTipo().equals(Configuracion.IdClase)) {
+				reportarError(
+						"El identificador corresponde a un identificador de clase",
+						tokenActual.getFila(), tokenActual.getColumna());
+				return new Argumento(tipoDato, identificadorVariable);
+
+			} else {
+				reportarError("Falta el nombre del parámetro",
+						tokenActual.getFila(), tokenActual.getColumna());
+				return new Argumento(tipoDato, identificadorVariable);
+			}
+
+		}
+	}
+
+	/**
+	 * Indica si cierto conjunto de tokens pertenecen a la categoria sintactica
+	 * Argumentos GIC: <Argumentos> ::= <Argumento> [ "," <Argumentos> ]
+	 * 
+	 * @return listaArgumentos
+	 */
+	public ArrayList<Argumento> esListaParametros() {
+		ArrayList<Argumento> listaArgumentos = new ArrayList<Argumento>();
+
+		Argumento argumento = esArgumento();
+
+		while (argumento != null) {
+			listaArgumentos.add(argumento);
+
+			if (tokenActual.getLexema() != SimboloLexico.SEPARADOR) {
+				break;
+			}
+			darSiguienteToken();
+
+			argumento = esParametro();
+		}
+
+		return listaArgumentos;
+	}
+
+	/**
+	 * Indica si cierto conjunto de tokens conforman La Unidad de Compilación
+	 * GIC: <UnidadDeCompilacion> ::=<Clase>
+	 * 
+	 * @return unidadCompilacion
+	 */
+	public UnidadDeCompilacion esUnidadDeCompilacion() {
+		Clase clase = esClase();
+
+		return new UnidadDeCompilacion(clase);
+	}
+
+	public Clase esClase(){
+
+		SimboloLexico modificadorAcceso = null;
+		SimboloLexico idClase = null;
+		CuerpoClase cuerpoClase = null;
+
+
+		modificadorAcceso = esModificadorAcceso();
+
+		if(modificadorAcceso == null){
+
+			reportarError("Falta modificador de acceso",tokenActual.getFila(), tokenActual.getColumna());
+
+		}
+
+
+		if(tokenActual.getTipo().equals(Configuracion.IdClase)){
+			idClase = tokenActual;
+			darSiguienteToken();
+		}
+
+		if(modificadorAcceso==null & idClase==null){
+			darSiguienteToken();
+
+			if(tokenActual.getTipo().equals(Configuracion.IdClase)){
+
+				idClase = tokenActual;
+				darSiguienteToken();	
+			}
+			else{
+				reportarError("falta el identificador de la clase",tokenActual.getFila(), tokenActual.getColumna());
+
+			}
+		}
+		else{
+			if(idClase==null){
+				reportarError("falta el identificador de la clase",tokenActual.getFila(), tokenActual.getColumna());
+			}
+		}
+
+		cuerpoClase = esCuerpoClase(); 
+		if(cuerpoClase != null){
+
+			return new Clase(modificadorAcceso, idClase, cuerpoClase);
+		}
+		else{
+			reportarError("Falta el cuerpo de la clase",tokenActual.getFila(), tokenActual.getColumna());
+		}
+		return new Clase(modificadorAcceso, idClase, cuerpoClase);
+	}
+
+	public CuerpoClase esCuerpoClase(){
+
+		ArrayList<DeclaracionVariable> listaDeclaraciones = esListaDeclaraciones();
+		ArrayList<Asignacion> listaAsignaciones = esListaAsignaciones();
+		ArrayList<DeclaracionMetodo>  listaMetodos = esListaMetodos();
+
+
+		return new CuerpoClase(listaDeclaraciones,listaAsignaciones,listaMetodos);
+
+	}
+
+	public ArrayList<DeclaracionVariable> esListaDeclaraciones(){
+
+		ArrayList<DeclaracionVariable> listaDeclaraciones = new ArrayList<DeclaracionVariable>();
+		DeclaracionVariable declaracionVariable = esDeclaracionVariable();
+
+		while(declaracionVariable != null){
+			listaDeclaraciones.add(declaracionVariable);
+			declaracionVariable = esDeclaracionVariable();
+		}
+
+		return listaDeclaraciones; 
+	}
+
+	public ArrayList<Asignacion> esListaAsignaciones(){
+
+		ArrayList<Asignacion> listaAsignacion = new ArrayList<Asignacion>();
+		Asignacion asignacion = esAsignacion();
+
+		while(asignacion != null){
+
+			listaAsignacion.add(asignacion);
+			asignacion = esAsignacion(); 
+		}
+
+		return listaAsignacion;
+
+	}
+
+	public ArrayList<DeclaracionMetodo> esListaMetodos(){
+
+		ArrayList<DeclaracionMetodo> listaMetodos = new ArrayList<DeclaracionMetodo>();
+		DeclaracionMetodo declaracionMetodo = esDeclaracionMetodo();
+		while (declaracionMetodo != null){
+			listaMetodos.add(declaracionMetodo);
+			declaracionMetodo = esDeclaracionMetodo();
+		}
+
+		return listaMetodos;
+	}
+
+	public DeclaracionMetodo esDeclaracionMetodo(){
+
+		SimboloLexico modificadorAcceso = null;
+		SimboloLexico tipoDato = null;
+		SimboloLexico idMetodo = null;
+		ArrayList<Argumento> argumentos = null;
+		SimboloLexico aperturaParentesis = null;
+		SimboloLexico cierreParentesis = null;
+
+		modificadorAcceso = esModificadorAcceso();
+
+
+
+
+
+
+
+
+
+	}
+
+	public DeclaracionVariable esDeclaracionVariable(){
+		int posBacktraking = indice;
+		SimboloLexico tipoDato = null;
+		ArrayList<SimboloLexico> idVariables = null;
+		SimboloLexico terminal = null;
+
+		tipoDato = esTipoDato();
+
+		idVariables = esVariables();
+
+		if(tokenActual.getTipo().equals(Configuracion.FinSentencia)){
+			terminal = tokenActual;
+		}
+
+		if(idVariables.size() > 0  && tipoDato !=null && terminal!=null){
+
+			return new DeclaracionVariable(tipoDato,idVariables,terminal);
+
+		}
+		
+		if(idVariables.size() > 0 && tipoDato!=null && terminal==null){
+			
+			modoPanico(";");
+		}
+		if(tipoDato == null && idVariables.size() > 0 && terminal!=null){
+
+			reportarError("falta el tipo de dato", tokenActual.getFila(), tokenActual.getColumna());
+
+			return new DeclaracionVariable(tipoDato,idVariables,terminal);	
+		}
+		realizarBactracking(posBacktraking);
+		return null;
+
+	}
+
+	public ArrayList<SimboloLexico> esVariables(){
+
+		ArrayList<SimboloLexico> idVariables = new ArrayList<SimboloLexico>();
+		SimboloLexico idVariable = esIdVariable();
+
+		while(idVariable != null){
+
+			idVariables.add(idVariable);
+
+			if(tokenActual.getTipo().equals(Configuracion.separador)){
+				darSiguienteToken();
+				idVariable = esIdVariable();
+			}
+			else{
+				if(tokenActual.getTipo().equals(Configuracion.IdVariable)){
+				
+				reportarError("falta el separador",tokenActual.getFila() , tokenActual.getColumna());
+				break;
+				}
+			}
+		}
+
+		return idVariables;
+
+	}
+
+	public SimboloLexico esIdVariable(){
+
+		SimboloLexico idVariable = null;
+
+		if(tokenActual.getTipo().equals(Configuracion.IdVariable)){
+			idVariable = tokenActual;
+			darSiguienteToken();
+
+		}
+
+		return idVariable;
+
+	}
+
+	public Asignacion esAsignacion(){
+		
+		int posBacktraking = indice;
+		SimboloLexico idVarible = null;
+		SimboloLexico asignacion = null;
+		SimboloLexico Valor = null;
+		ExpresionComparacion exprecionComparacion = null;
+		ExprecionMatematica exprecionMatematica = null;
+		
+		
+		
+
+		
+		
+		
+	}
+
+	private SimboloLexico esModificadorAcceso(){
+
+		SimboloLexico modificadorAcceso = null;
+
+		if(tokenActual.getLexema().equals("<+>") || tokenActual.getLexema().equals("<->") ){
+
+			modificadorAcceso = tokenActual;
+			darSiguienteToken();
+			return modificadorAcceso;
+
+		}
+		else{
+
+
+			return modificadorAcceso;
+		}
+	}
+
+	private SimboloLexico esTipoDato(){
+		SimboloLexico tipoDato = null;
+
+		if (tokenActual.getLexema().equals(Configuracion.Real)
+				|| tokenActual.getLexema().equals(Configuracion.Entero)
+				|| tokenActual.getLexema().equals(Configuracion.Cadena)) {
+			tipoDato = tokenActual;
+			darSiguienteToken();
+			return tipoDato;
+		} else {
+
+
+			return tipoDato;
+		}
+	}
+public void modoPanico(String tokenParada) {
+		
+		while (!tokenActual.getLexema().equals(tokenParada)) {
+				darSiguienteToken();
+			}
+			
+		}
 }
