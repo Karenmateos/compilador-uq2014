7fd62dfed3e5b4bafef603dc0292694a65a7ea5a
lado396 <lado396@77e49527-3367-abf7-0bbe-53a63c11a891>
2014-04-30 17:48:43 +0000

    Clase DeclaracionMetodo

--- src/analizadorSintactico/DeclaracionMetodo.java
+++ src/analizadorSintactico/DeclaracionMetodo.java
@@ -1,5 +1,72 @@
 package analizadorSintactico;
 
-public class DeclaracionMetodo {
+import java.util.ArrayList;
 
+import javax.swing.tree.DefaultMutableTreeNode;
+
+import analizadorLexico.SimboloLexico;
+
+public class DeclaracionMetodo {
+	
+	SimboloLexico modificadorAcceso;
+	SimboloLexico tipoDato;
+	SimboloLexico idMetodo;
+	SimboloLexico aperturaParentesis;
+	ArrayList<Argumento> argumentos;
+	SimboloLexico cierreParentesis;
+	CuerpoMetodo cuerpoMetodo;
+	
+	public DeclaracionMetodo(SimboloLexico modificadorAcceso, SimboloLexico tipoDato, SimboloLexico idMetodo, SimboloLexico aperturaParentesis, ArrayList<Argumento> argumentos, SimboloLexico cierreParentesis, CuerpoMetodo cuerpoMetodo){
+		this.modificadorAcceso = modificadorAcceso;
+		this.tipoDato = tipoDato;
+		this.idMetodo = idMetodo;
+		this.aperturaParentesis = aperturaParentesis;
+		this.argumentos = argumentos;
+		this.cierreParentesis = cierreParentesis;
+		this.cuerpoMetodo = cuerpoMetodo;
+	}
+	
+	public DefaultMutableTreeNode getArbolVisual()
+	{
+		DefaultMutableTreeNode miRaiz = new DefaultMutableTreeNode("Declaracion Metodo");
+
+		if(modificadorAcceso != null)
+		{
+			miRaiz.add(new DefaultMutableTreeNode(modificadorAcceso.getLexema()));
+		}
+		
+		if(tipoDato != null){
+			miRaiz.add(new DefaultMutableTreeNode(tipoDato.getLexema()));
+		}
+		
+		if(idMetodo != null){
+			miRaiz.add(new DefaultMutableTreeNode(idMetodo.getLexema()));
+		}
+		
+		if(aperturaParentesis != null){
+			miRaiz.add(new DefaultMutableTreeNode(aperturaParentesis.getLexema()));
+		}
+		
+		if(argumentos !=null || argumentos.size() > 0)
+		{		
+			DefaultMutableTreeNode aNode = new DefaultMutableTreeNode("Argumentos");
+
+			for (Argumento argumento : argumentos) 
+			{
+				aNode.add(argumento.getArbolVisual());
+			}
+
+			miRaiz.add(aNode);
+		}
+		
+		if(cierreParentesis != null){
+			miRaiz.add(new DefaultMutableTreeNode(cierreParentesis.getLexema()));
+		}
+		
+		if(cuerpoMetodo != null){
+			miRaiz.add(cuerpoMetodo.getArbolVisual());
+		}
+
+		return miRaiz;
+	}
 }
