4379d6ee6405c5c008d33bec59ecb16e9ddcd5f4
radiohead225l <radiohead225l@77e49527-3367-abf7-0bbe-53a63c11a891>
2014-04-30 14:09:46 +0000

    Operacion

---
+++ branches/analizadorLexicoTLF2/src/analizadorSintactico/Operacion.java
@@ -0,0 +1,43 @@
+package analizadorSintactico;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+
+import analizadorLexico.SimboloLexico;
+
+public class Operacion {
+
+	SimboloLexico operadorMatematico = null;
+	SimboloLexico idVariable = null;
+	Operacion operacion = null;
+
+	public Operacion(SimboloLexico operadorMatematico, SimboloLexico idVariable){
+
+		this.operadorMatematico = operadorMatematico;
+		this.idVariable = idVariable;
+	}
+
+	public Operacion(SimboloLexico operadorMatematico, Operacion operacion){
+
+		this.operadorMatematico = operadorMatematico;
+		this.operacion = operacion;
+	}
+
+	public DefaultMutableTreeNode getArbolVisual(){
+
+		DefaultMutableTreeNode miRaiz = new DefaultMutableTreeNode("Operacion");
+
+		miRaiz.add(new DefaultMutableTreeNode(operadorMatematico.getTipo()+": "+ operadorMatematico.getLexema()));
+
+		if(idVariable!=null){
+			miRaiz.add(new DefaultMutableTreeNode(idVariable.getTipo()+": "+ idVariable.getLexema()) );
+			return miRaiz;
+		}
+
+		if(operacion != null){
+
+			miRaiz.add(operacion.getArbolVisual());
+			return miRaiz;
+		}
+		return miRaiz;
+	}
+}
