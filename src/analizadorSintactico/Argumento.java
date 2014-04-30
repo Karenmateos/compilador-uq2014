8c1057934593ea84027cc4ebcd6a81ffd54bc1f2
lado396 <lado396@77e49527-3367-abf7-0bbe-53a63c11a891>
2014-04-30 15:10:44 +0000

    Clase Argumento completa

--- src/analizadorSintactico/Argumento.java
+++ src/analizadorSintactico/Argumento.java
@@ -1,5 +1,31 @@
 package analizadorSintactico;
 
-public class Argumento {
+import javax.swing.tree.DefaultMutableTreeNode;
 
+import analizadorLexico.SimboloLexico;
+
+public class Argumento {
+	SimboloLexico tipoDato;
+	SimboloLexico idVariable;
+	
+	public Argumento(SimboloLexico tipoDato, SimboloLexico idVariable){
+		this.tipoDato = tipoDato;
+		this.idVariable = idVariable;
+	}
+	
+	public DefaultMutableTreeNode getArbolVisual()
+	{
+		DefaultMutableTreeNode miRaiz = new DefaultMutableTreeNode("Argumento");
+
+		if(tipoDato != null)
+		{
+			miRaiz.add(new DefaultMutableTreeNode(tipoDato.getLexema()));
+		}
+		
+		if(idVariable != null){
+			miRaiz.add(new DefaultMutableTreeNode(idVariable.getLexema()));
+		}
+		
+		return miRaiz;
+	}
 }
